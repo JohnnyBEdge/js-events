4c770a7e873e1060c49a73dc28052800a83a41f9
John Martin <jrmartin182@gmail.com>
2020-03-19 09:50:50 -0600

    connected html to js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Javascript Events");
+
+const family = document.getElementById('family');
+const nsfw = document.getElementById('nsfw');
+const water = document.getElementById('water');
+const tupperware = document.getElementById('tupperware');
+const my_input = document.getElementById('my_input');
+const response = document.getElementById('response');
